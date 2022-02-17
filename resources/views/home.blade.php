01faa9ac61de6d32000fe01b285b19678d908e6e
ChriEffe <christianferrazzano93@gmail.com>
2022-02-17 15:34:32 +0100

    stampo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,10 @@
 @section('content')
     @foreach ($movies as $movie)
     <ul>
-        <li>
-            <h1>{{ $movie->title }}</h1>
-        </li>
+        <li><h1>Titolo: {{ $movie->title }}</h1></li>
+        <li><h2>Nazionalità: {{ $movie->nationality }}</h2></li>
+        <li><h2>Data uscita: {{ $movie->date }}</h2></li>
+        <li><h2>Voto: {{ $movie->vote }}</h2></li>
     </ul>  
     @endforeach
 @endsection
