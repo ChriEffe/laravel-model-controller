0cbcff99f573fe3b8a8ce058b28e9fd0bcb70f92
ChriEffe <christianferrazzano93@gmail.com>
2022-02-17 14:20:26 +0100

    home, base

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all();
+        $data = ['movies => $movies'];
+        return view('home', $data);
+    }
 }
